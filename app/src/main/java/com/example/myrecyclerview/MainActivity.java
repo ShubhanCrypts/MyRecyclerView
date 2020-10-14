b75a784ba987fa6ec53bd2e3eed699d98b47f9d6
ShubhanCrypts <Shubhan1901@gmail.com>
2020-10-14 22:26:29 +0700

    first commit

---
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.myrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setActionBarTitle(title);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecycleList();
+    }
+
+    private void showRecycleList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+
+        listHeroAdapter.setOnItemClickCallback(new ListHeroAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Hero data) {
+                showSelectedHero(data);
+            }
+        });
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.manu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode){
+        switch (selectedMode){
+            case R.id.action_list:
+                title = "Mode List";
+                showRecycleList();
+            break;
+
+            case R.id.action_grid:
+                title = "Mode Grid";
+                showRecycleGrid();
+            break;
+
+            case R.id.action_cardview:
+                title = "Mode CardView";
+                showRecycleCardView();
+            break;
+        }
+        setActionBarTitle(title);
+    }
+
+    private void showRecycleGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+
+        gridHeroAdapter.setOnItemClickCallback(new GridHeroAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Hero data) {
+                showSelectedHero(data);
+            }
+        });
+    }
+
+    private void showRecycleCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewHeroAdapter);
+    }
+
+    private void setActionBarTitle(String title){
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    private void showSelectedHero(Hero hero){
+        Toast.makeText(this, "Kamu memilih" + hero.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+
+}
